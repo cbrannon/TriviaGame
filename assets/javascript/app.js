0949944b0f4361212cd797a75e1f4c063310c575
Cody Brannon <codydbrannon@gmail.com>
2017-02-22 23:13:23 -0500

    Methods for setting question and randomize answer location.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,72 @@
 $(document).ready(function() {
     var trivia = {
-        questions: {
 
-        }
+        questions: {
+            starks: {
+                question: "Who dead?",
+                answer: "The Starks",
+                options: ["The Lannisters", "The Lannisters",
+                    "The Lannisters", "The Starks"
+                ],
+                answerImage: "#"
+            },
+
+
+        },
 
         correctAnswers: 0,
         incorrectAnswers: 0,
         unanswered: 0,
-        question: null,
+        currentQuestion: null,
 
         setupGame: function() {
+            this.setQuestion();
+        },
 
-        }
-
+        // Choose a random question from quetions object and set our variable.
         setQuestion: function() {
             var objKeys = Object.keys(this.questions);
-            this.question = objKeys[Math.floor(Math.random() * objKeys.length)];
-
+            this.currentQuestion = this.questions[objKeys[Math.floor(Math.random() * objKeys.length)]];
+            console.log(this.currentQuestion);
             this.setPlayArea();
-        }
+        },
 
         restartGame: function() {
-
-        }
+            this.correctAnswers = 0;
+            this.incorrectAnswers = 0;
+            this.unanswered = 0;
+            this.currentQuestion = null;
+        },
 
         setPlayArea: function() {
+            var questionArray = this.currentQuestion.options;
+            // Shuffle answer options.
+            shuffle(questionArray);
+            // Set answer options to DOM.
+            for (var optionIndex = 0; optionIndex < questionArray.length; optionIndex++) {
+                var questionToSet = questionArray[optionIndex],
+                    optionLocation = "#option-" + (optionIndex + 1);
+                $(optionLocation).text(questionToSet);
+            }
+            // Set question to DOM.
+            $("#question").text(this.currentQuestion.question);
+
+            function shuffle(a) {
+                var j, x, i;
+                for (i = a.length; i; i--) {
+                    j = Math.floor(Math.random() * i);
+                    x = a[i - 1];
+                    a[i - 1] = a[j];
+                    a[j] = x;
+                }
+            }
+        },
+
+        updatePlayArea: function() {
 
         }
     }
+
+    trivia.setupGame();
+
 });
