c08e1a9615721a0c53023ae8acfe41cfc659a783
Cody Brannon <codydbrannon@gmail.com>
2017-02-23 00:17:19 -0500

    Work on timing functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,14 +4,20 @@ $(document).ready(function() {
         questions: {
             starks: {
                 question: "Who dead?",
-                answer: "The Starks",
+                correctAnswer: "The Starks",
                 options: ["The Lannisters", "The Lannisters",
                     "The Lannisters", "The Starks"
                 ],
                 answerImage: "#"
             },
-
-
+            lannisters: {
+                question: "Who wins?",
+                correctAnswer: "The Lannisters",
+                options: ["The Starks", "The Starks",
+                    "The Lannisters", "The Starks"
+                ],
+                answerImage: "#"
+            }
         },
 
         correctAnswers: 0,
@@ -29,6 +35,7 @@ $(document).ready(function() {
             this.currentQuestion = this.questions[objKeys[Math.floor(Math.random() * objKeys.length)]];
             console.log(this.currentQuestion);
             this.setPlayArea();
+            this.timer();
         },
 
         restartGame: function() {
@@ -36,6 +43,9 @@ $(document).ready(function() {
             this.incorrectAnswers = 0;
             this.unanswered = 0;
             this.currentQuestion = null;
+            for (var optionIndex = 1; optionIndex <= currentQuestion.options.length; optionIndex++) {
+                $("#option-" + optionIndex).text("");
+            }
         },
 
         setPlayArea: function() {
@@ -44,12 +54,12 @@ $(document).ready(function() {
             shuffle(questionArray);
             // Set answer options to DOM.
             for (var optionIndex = 0; optionIndex < questionArray.length; optionIndex++) {
-                var questionToSet = questionArray[optionIndex],
-                    optionLocation = "#option-" + (optionIndex + 1);
-                $(optionLocation).text(questionToSet);
+                $("#option-" + (optionIndex + 1)).text(questionArray[optionIndex]);
             }
             // Set question to DOM.
             $("#question").text(this.currentQuestion.question);
+
+            $("#timeLeft").text("5");
 
             function shuffle(a) {
                 var j, x, i;
@@ -62,11 +72,50 @@ $(document).ready(function() {
             }
         },
 
-        updatePlayArea: function() {
+        getAnswer: function(condition) {
+            if (condition == "Out of Time") {
+                this.unanswered++;
+                // $("#timer").text(condition);
+                console.log("OOT");
+            } else if (condition == this.currentQuestion.correctAnswer) {
+                this.correctAnswers++;
+                // $("#timer").text("Correct!");
+                console.log("Correct answers updated to: " + this.correctAnswers);
+            } else {
+                this.incorrectAnswers++;
+                // $("#timer").text("Wrong!");
+                console.log("Incorrect answers updated to: " + this.incorrectAnswers);
+            }
 
+            this.setQuestion();
+        },
+
+        timer: function() {
+            var count = 5;
+            var counter = setInterval(start, 1000);
+            var self = this;
+
+
+            function start() {
+                count = count - 1;
+                $("#timeLeft").text(count);
+                if (count <= 0) {
+                    clearInterval(counter);
+                    self.getAnswer("Out of Time");
+                    //counter ended, do something here
+                    return;
+                }
+                //Do code for showing the number of seconds here
+            }
         }
     }
 
     trivia.setupGame();
+
+    $(".option").on("click", function() {
+        var answer = $(this).text();
+        console.log(answer);
+        trivia.getAnswer(answer);
+    });
 
 });
